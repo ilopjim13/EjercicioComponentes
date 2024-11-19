56a4c1f5e19704658d8e76e0aec3851050f9c91d
ivanjim13 <ivangorrion57@gmail.com>
2024-11-19 04:34:54 +0100

    seguir poniendo bonito

--- app/src/main/java/com/example/ejerciciocomponentes/MainActivity.kt
+++ app/src/main/java/com/example/ejerciciocomponentes/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxColors
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -20,10 +30,16 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ejerciciocomponentes.ui.theme.EjercicioComponentesTheme
 
 class MainActivity : ComponentActivity() {
@@ -55,33 +71,52 @@ fun ComponentesBasicos(name: String, modifier: Modifier = Modifier) {
 
     val checkeados = remember { mutableStateListOf<Int>() }
     Column(modifier.fillMaxSize()) {
-        Row {
-            Text(text = "Esta es mi aplicacion")
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .background(Color.DarkGray),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Selector", modifier.padding(start = 16.dp), fontWeight = FontWeight.Bold, fontSize = 28.sp, color = Color.White)
+            Icon(
+                imageVector = Icons.Filled.MoreVert,
+                contentDescription = "opciones",
+                modifier.padding(end = 16.dp),
+                tint = Color.White
+            )
         }
 
-        Column {
-            Row {
+        Spacer(Modifier.fillMaxWidth().height(1.dp).border(1.dp, Color.Black))
+
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 40.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Text("Elige los objetivos", modifier = Modifier.padding(bottom = 16.dp))
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Checkbox(checked = check1, onCheckedChange = {
                     check1 = !check1
-                    if(check1) checkeados.remove(1)
+                    if(!check1) checkeados.remove(1)
                     else checkeados.add(1)
                     button = false
                 })
                 Text(text = "Objetivo 1")
             }
-            Row {
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Checkbox(checked = check2, onCheckedChange = {
                     check2 = !check2
-                    if(check2) checkeados.remove(2)
+                    if(!check2) checkeados.remove(2)
                     else checkeados.add(2)
                     button = false
                 })
                 Text(text = "Objetivo 2")
             }
-            Row {
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 Checkbox(checked = check3, onCheckedChange = {
                     check3 = !check3
-                    if(check3) checkeados.remove(3)
+                    if(!check3) checkeados.remove(3)
                     else checkeados.add(3)
                     button = false
                 })
@@ -92,12 +127,12 @@ fun ComponentesBasicos(name: String, modifier: Modifier = Modifier) {
             Switch(checked = switch, onCheckedChange = {
                 switch =  !switch
                 button = false
-            })
+            }, modifier = Modifier.padding(top = 20.dp))
 
 
 
             if (switch) {
-                Button(onClick = { button = !button }) {
+                Button(onClick = { button = !button }, Modifier.padding(bottom = 56.dp, top = 20.dp)) {
                     Text("Pulsar")
                 }
 
@@ -107,11 +142,22 @@ fun ComponentesBasicos(name: String, modifier: Modifier = Modifier) {
         }
 
 
+
         if (button && switch) {
-            val objetivo = checkeados.minOrNull()
-            Column()  {
+            Column(
+                Modifier
+                    .width(200.dp)
+                    .height(120.dp)
+                    .border(1.dp, Color.Black)
+                    .background(Color.LightGray)
+                    .align(Alignment.CenterHorizontally),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                val objetivo = checkeados.minOrNull()
                 Text(
-                    text = when(objetivo) {
+                    text = when (objetivo) {
                         1 -> "Objetivo 1"
                         2 -> "Objetivo 2"
                         3 -> "Objetivo 3"
@@ -119,9 +165,7 @@ fun ComponentesBasicos(name: String, modifier: Modifier = Modifier) {
                     }
                 )
             }
-
         }
-
 
 
 
